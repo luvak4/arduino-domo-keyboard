87c48530626960929f516e1df790722aa994afb9
luvak4 <luvak4@gmail.com>
2016-10-13 13:44:37 +0200

    Update arduino-domotic-keyboard.ino

--- arduino-domotic-keyboard.ino
+++ arduino-domotic-keyboard.ino
@@ -1,9 +1,12 @@
 #include <IRremote.h>        // load ir library
 #include <VirtualWire.h>     // load virtual-wire library
+int  INTERIlocali[4]={0,0,0,0};
+byte BYTEradio[8];
+byte CIFR[8]={156,33,183,95,230,63,250,215};
+const unsigned long mask=0x0000FFFF;
 const int led_pin = 13;      // led pin
-//const int receive_pin = 11;  // radio rx pin
+const int receive_pin = 11;  // radio rx pin
 const int transmit_pin = 12; // radio tx pin
-const int MSG_LEN = 7;       // radio max message lenght
 const int RECV_PIN = 2;      // ir pin
 IRrecv irrecv(RECV_PIN);     // ir initialize library
 decode_results results;      // ir variable
@@ -14,9 +17,25 @@ const long responseA[]={16753245,16736925,16769565,\
 			16724175,16718055,16743045,\
 			16716015,16726215,16734885,\
 			16728765,16730805,16732845};
-const byte KEY_OK = 21;
+#define KEY_OK     3
+#define KEY_1     13
+#define KEY_2     14
+#define KEY_3     15
+#define KEY_4     16
+#define KEY_5     17
+#define KEY_6     18
+#define KEY_7     19
+#define KEY_8     20
+#define KEY_9     21
+#define KEY_0     10
+#define INDIRIZZO  0
+#define DATOa      1
+#define DATOb      2
+#define DATOc      3
+
 unsigned long tempo=0;
 int NumeroComposto=0;
+
 ////////////////////////////////
 // setup
 ////////////////////////////////
@@ -35,13 +54,55 @@ void setup()
 // loop
 ////////////////////////////////
 void loop(){
-  if (irrecv.decode(&results)) {        // got ir signal
+  if (irrecv.decode(&results)) {  // got ir signal
     byte key=ir_decode(&results); // usable?
+
+
+    
+    
     if (key>0){
       //
       delay(100);
       tempo=millis();
       //
+      switch (key){
+      case KEY_OK:
+	break;
+      case KEY_1:
+	NumeroComposto=NumeroComposto*10+1;
+	break;
+      case KEY_2:
+	NumeroComposto=NumeroComposto*10+2;
+	break;
+      case KEY_3:
+	NumeroComposto=NumeroComposto*10+3;
+	break;
+      case KEY_4:
+	NumeroComposto=NumeroComposto*10+4;	
+	break;
+      case KEY_5:
+	NumeroComposto=NumeroComposto*10+5;
+	break;
+      case KEY_6:
+	NumeroComposto=NumeroComposto*10+6;	
+	break;
+      case KEY_7:
+	NumeroComposto=NumeroComposto*10+7;	
+	break;
+      case KEY_8:
+	NumeroComposto=NumeroComposto*10+8;	
+	break;
+      case KEY_9:
+	NumeroComposto=NumeroComposto*10+9;	
+	break;
+      case KEY_0:
+	NumeroComposto=NumeroComposto*10;
+	break;
+      }
+
+
+
+      
       if (key==KEY_OK){
 	// premuto carattere di conferma
 	//////////////////////////////////
@@ -80,6 +141,11 @@ void loop(){
       }
     }
     irrecv.resume();                    // ir-receive resume
+
+
+
+
+    
   }
 }
 
@@ -94,4 +160,34 @@ byte ir_decode(decode_results *results){
     }
   }
   return key;                           // return value
+}
+
+void decodeMessage(){
+  // de-cifratura
+  byte m=0;
+  cipher();
+  for (int n=0; n<4;n++){
+    INTERIlocali[n]=BYTEradio[m+1];
+    INTERIlocali[n]=INTERIlocali[n] << 8;
+    INTERIlocali[n]=INTERIlocali[n]+BYTEradio[m];
+    m+=2;
+  }
+}
+
+void encodeMessage(){
+  // from struct to byte
+  byte m=0;
+  for (int n=0; n<4;n++){
+    BYTEradio[m]=INTERIlocali[n] & mask;
+    INTERIlocali[n]=INTERIlocali[n] >> 8;
+    BYTEradio[m+1]=INTERIlocali[n] & mask;
+    m+=2;
+  }
+  cipher();
+}
+
+void cipher(){
+  for (byte n=0;n<8;n++){
+    BYTEradio[n]=BYTEradio[n]^CIFR[n];
+  }
 }

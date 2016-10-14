770078f32a540cb745f6fdd03778d3511dd4d178
luvak4 <luvak4@gmail.com>
2016-10-14 11:11:57 +0200

    Update arduino-domotic-keyboard.ino

--- arduino-domotic-keyboard.ino
+++ arduino-domotic-keyboard.ino
@@ -1,16 +1,18 @@
-#include <IRremote.h>        // load ir library
-#include <VirtualWire.h>     // load virtual-wire library
-#define KEY_OK     3
-#define KEY_1     13
-#define KEY_2     14
-#define KEY_3     15
-#define KEY_4     16
-#define KEY_5     17
-#define KEY_6     18
-#define KEY_7     19
-#define KEY_8     20
-#define KEY_9     21
-#define KEY_0     10
+////////////////////////////////#include <IRremote.h> 
+#include <VirtualWire.h>
+//
+#define KEY_OK    16769565
+#define KEY_1     16724175
+#define KEY_2     16718055
+#define KEY_3     16743045
+#define KEY_4     16716015
+#define KEY_5     16726215
+#define KEY_6     16734885
+#define KEY_7     16728765
+#define KEY_8     16730805
+#define KEY_9     16732845
+#define KEY_0     16738455
+//
 #define INDIRIZZO  0
 #define DATOa      1
 #define DATOb      2
@@ -25,57 +27,54 @@ int  INTERIlocali[4]={0,0,0,0};
 byte BYTEradio[BYTEStoTX];
 byte CIFR[BYTEStoTX]={156,33,183,95,230,63,250,215};
 const unsigned long mask=0x0000FFFF;
-const int led_pin = 13;      // led pin
-const int power_radio_rx =10; //alimentazione rx
-const int receive_pin = 11;  // radio rx pin
-const int transmit_pin = 12; // radio tx pin
-uint8_t buflen = BYTEStoTX;    //for rx
+//
+const int led_pin        = 13;      
+const int power_radio_rx = 10;
+const int receive_pin    = 11;
+const int transmit_pin   = 12;
+uint8_t buflen = BYTEStoTX;  //for rx
 const int RECV_PIN = 2;      // ir pin
-IRrecv irrecv(RECV_PIN);     // ir initialize library
-decode_results results;      // ir variable
-const long responseA[]={16753245,16736925,16769565,\
-			16720605,16712445,16761405,\
-			16769055,16754775,16748655,\
-			16738455,16750695,16756815,\
-			16724175,16718055,16743045,\
-			16716015,16726215,16734885,\
-			16728765,16730805,16732845};
-
-
+////////////////////////////////IRrecv irrecv(RECV_PIN);     // ir initialize library
+////////////////////////////////decode_results results;      // ir variable
+/*
+  16753245 16736925 16769565
+  16720605 16712445 16761405
+  16769055 16754775 16748655
+  16738455 16750695 16756815
+  16724175 16718055 16743045
+  16716015 16726215 16734885
+  16728765 16730805 16732845
+*/
 unsigned long tempo=0;
 int NumeroComposto=0;
-
 ////////////////////////////////
 // setup
 ////////////////////////////////
 void setup()
 {
-  pinMode(led_pin, OUTPUT);    // led set pin
-  pinMode(power_radio_rx, OUTPUT);    // led set pin
+  pinMode(led_pin, OUTPUT);
+  pinMode(power_radio_rx, OUTPUT);
   digitalWrite(power_radio_rx,HIGH);
   vw_set_tx_pin(transmit_pin); // radio set tx pin
   vw_set_rx_pin(receive_pin);  // radio set rx pin
   vw_setup(1000);              // radio speed
   vw_rx_start();               // radio rx ON
-  irrecv.enableIRIn();         // ir rx ON
+  ////////////////////////////////irrecv.enableIRIn();         // ir rx ON
   Serial.begin(9600);
 }
-
 ////////////////////////////////
 // loop
 ////////////////////////////////
 void loop(){
-  testIR();
-  //////////////
+  ////////////////////////////////testIR();
+  //
   if (vw_get_message(BYTEradio, &buflen)){
     decodeMessage();
     Serial.println("----");
     if (INTERIlocali[INDIRIZZO]==CIRC_CANTINA){
-      
     }
   } 
-  
-  /////////////
+  //
   if ((millis()-tempo)>5000){
     tempo=millis();
     NumeroComposto=0;
@@ -85,18 +84,12 @@ void loop(){
   }
 }
 
-byte ir_decode(decode_results *results){
+/*////////////////////////////////
+long ir_decode(decode_results *results){
   long keyLongNumber = results->value;  // get the long number
-  //Serial.println(keyLongNumber);
-  byte key=0;
-  for (byte r=0; r<21 ; r++){           // search in array
-    if (responseA[r]==keyLongNumber){   // found?
-      key=r+1;                          // key in 1-21 range
-      break;
-    }
-  }
-  return key;                           // return value
+  return keyLongNumber;
 }
+*/////////////////////////////////
 
 void decodeMessage(){
   // de-cifratura
@@ -128,20 +121,15 @@ void cipher(){
   }
 }
 
+/*////////////////////////////////
 void testIR(){
-  if (irrecv.decode(&results)) {  // got ir signal
-byte key=ir_decode(&results); // usable?
-////// if key
-if (key>0){
-  //
-
-  delay(100);
-  tempo=millis();
-  //
-  //Serial.println(key);
-  switch (key){
+  if (irrecv.decode(&results)) {
+    long key=ir_decode(&results); 
+    //
+    //Serial.println(key);
+    switch (key){
     case KEY_OK:
-    Serial.println(NumeroComposto);
+      //Serial.println(NumeroComposto);
       INTERIlocali[INDIRIZZO]=PONTEsuGIU;
       INTERIlocali[DATOa]=NumeroComposto;
       INTERIlocali[DATOb]=0;
@@ -157,42 +145,41 @@ if (key>0){
       digitalWrite(led_pin,LOW);
       //Serial.println(NumeroComposto);
       NumeroComposto=0;
-    break;
+      break;
     case KEY_1:
-    NumeroComposto=NumeroComposto*10+1;
-    break;
+      NumeroComposto=NumeroComposto*10+1;
+      break;
     case KEY_2:
-    NumeroComposto=NumeroComposto*10+2;
-    break;
+      NumeroComposto=NumeroComposto*10+2;
+      break;
     case KEY_3:
-    NumeroComposto=NumeroComposto*10+3;
-    break;
+      NumeroComposto=NumeroComposto*10+3;
+      break;
     case KEY_4:
-    NumeroComposto=NumeroComposto*10+4;  
-    break;
+      NumeroComposto=NumeroComposto*10+4;  
+      break;
     case KEY_5:
-    NumeroComposto=NumeroComposto*10+5;
-    break;
+      NumeroComposto=NumeroComposto*10+5;
+      break;
     case KEY_6:
-    NumeroComposto=NumeroComposto*10+6; 
-    break;
+      NumeroComposto=NumeroComposto*10+6; 
+      break;
     case KEY_7:
-    NumeroComposto=NumeroComposto*10+7; 
-    break;
+      NumeroComposto=NumeroComposto*10+7; 
+      break;
     case KEY_8:
-    NumeroComposto=NumeroComposto*10+8; 
-    break;
+      NumeroComposto=NumeroComposto*10+8; 
+      break;
     case KEY_9:
-    NumeroComposto=NumeroComposto*10+9; 
-    break;
+      NumeroComposto=NumeroComposto*10+9; 
+      break;
     case KEY_0:
-    NumeroComposto=NumeroComposto*10;
-    break;
+      NumeroComposto=NumeroComposto*10;
+      break;
+    }
+    delay(100);
+    tempo=millis();
+    irrecv.resume();
   }
-                  // ir-receive resume
 }
-  irrecv.resume();
-///// end if key
-}
-
-}
+*/////////////////////////////////
